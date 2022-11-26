9acd1033eadd6370892fa91645a9db19d8c93a04
rrrussrrr <rruebner@gmail.com>
2022-11-25 22:45:30 -0500

    added Material UI as dependency, using to make stock card template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
+import StockTab from './components/StockTab';
+import StockTable from './components/StockDisplay';
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 
 function App() {
+
+  const [stockTickers, setStockTickers] = useState([
+    {ticker: "AAPL"},{ticker:"GOOG"}
+  ]);
+
+
+
   return (
     <div className="App">
 
+
+      <StockTable stockList={stockTickers}/>
+      <Button>Cliq</Button>
     </div>
   );
 }
--- frontend/src/components/StockTab.js
+++ frontend/src/components/StockTab.js
@@ -1,9 +1,25 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 const StockTab = ({stockData}) => {
     return (
-        <div>
+        <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h5" component="div">
             {stockData.ticker}
-        </div>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
     )
+
+
+
 }
 
 export default StockTab
