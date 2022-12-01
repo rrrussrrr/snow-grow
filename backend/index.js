c5ed93560425db6131c8c3f532cb850d2173f4b6
rrrussrrr <rruebner@gmail.com>
2022-11-30 19:00:14 -0500

    got all data to pull from polygon, testing interval

--- backend/index.js
+++ backend/index.js
@@ -5,21 +5,227 @@ const logger = require('./utils/logger')
 const server = http.createServer(app);
 const axios = require('axios')
 const https = require ('node:https')
-
-
+const fetch = require("node-fetch");
+const TickerCloseData = require ('./models/tickerclosedata')
+//
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
+
+
+
+const el = (async () => {
+    console.log("grabbing")
+    const response = await fetch('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2022-10-14?adjusted=true&include_otc=false&apiKey=76hxIfZRCopscD2ZGWchjaDppLYEjDvn');
+    const data = await response.json();
+    
+
+    const formatted = data.results.map(item => new TickerCloseData({
+            ticker: item.T,
+            volume: item.v,
+            volumeWeightedAerage: item.vw,
+            open: item.o,
+            close: item.c,
+            highest:item.h,
+            lowest:item.l,
+            timeStart: item.t,
+            numOfTrades: item.n
+          }))
+        
+            TickerCloseData.collection.insertMany(formatted);
+});
+
+const intervalID = setInterval(el, 5000);
+
+setInterval(() => {
+    console.log("clearing")
+clearInterval(intervalID)
+}, 15000)
+
 
 //make periodic API call request
 
 //check once
 
-axios
-.get('https://api.polygon.io/v1/open-close/AAPL/2022-10-14?adjusted=true&apiKey=FBY1RCAjzXNWT6sIyD9XntH6qpp1fWRn')
-.then(response => {
-  console.log(response.data)
-})
+// axios
+// .get('https://api.polygon.io/v1/open-close/AAPL/2022-10-14?adjusted=true&apiKey=FBY1RCAjzXNWT6sIyD9XntH6qpp1fWRn')
+// .then(async (response) => {
+//   const tickerData = new TickerCloseData({
+//     ticker: response.data.symbol
+//   })
+
+
+//   try {
+//     const savedData = await tickerData.save()
+//     response.status(201).json(savedData);
+//   } catch(exception) {
+
+//   }
+// })
+
+// axios
+// .get('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2022-10-14?adjusted=true&include_otc=false&apiKey=76hxIfZRCopscD2ZGWchjaDppLYEjDvn')
+// .then(async (response) => {
+
+//   response.data.results.forEach(ticker => {
+//     const tickerData = new TickerCloseData({
+//       ticker: ticker.T
+//     })
+//     try {
+//       const savedData = await tickerData.save()
+//       response.status(201).json(savedData);
+//     } catch(exception) {
+//     }
+//   })
+
+
+// # 4
+// axios
+// .get('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2022-10-14?adjusted=true&include_otc=false&apiKey=76hxIfZRCopscD2ZGWchjaDppLYEjDvn', {data: {}})
+// .then(async (response) => {
+
+
+//   // if (response.data.results) {
+//   //   console.log(response.data.results)
+//   // }
+//   const formatted = response.data.results.map(item => new TickerCloseData({
+//     ticker: item.T,
+//     volume: item.v,
+//     volumeWeightedAerage: item.vw,
+//     open: item.o,
+//     close: item.c,
+//     highest:item.h,
+//     lowest:item.l,
+//     timeStart: item.t,
+//     numOfTrades: item.n
+//   }))
+
+//   try {
+//     TickerCloseData.collection.insertMany(formatted);
+//   } catch(exception) {
+
+//   }
+//   // try {
+//   //   const savedData = await tickerData.save()
+//   //   response.status(201).json(savedData);
+//   // } catch(exception) {
+
+//   // }
+// })
+
+// axios
+// .get('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2022-10-14?adjusted=true&include_otc=false&apiKey=76hxIfZRCopscD2ZGWchjaDppLYEjDvn', (req, res) => {
+//   console.log(res.data.results[0])
+//   const formatted = res.data.results.map(item => new TickerCloseData({
+//     ticker: item.T,
+//     volume: item.v,
+//     volumeWeightedAerage: item.vw,
+//     open: item.o,
+//     close: item.c,
+//     highest:item.h,
+//     lowest:item.l,
+//     timeStart: item.t,
+//     numOfTrades: item.n
+//   }))
+//   TickerCloseData.collection.insertMany(formatted);
+
+// })
+
+// function axiosGet() {
+//   return axios.get('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2022-10-14?adjusted=true&include_otc=false&apiKey=76hxIfZRCopscD2ZGWchjaDppLYEjDvn')
+
+// }
+
+// async function func  () {
+//   let dddata = await axiosGet();
+//   console.log(Object.keys(dddata.request))
+
+// }
+
+// axios
+// .get('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2022-10-14?adjusted=true&include_otc=false&apiKey=76hxIfZRCopscD2ZGWchjaDppLYEjDvn', {responseType: 'json'})
+// .then(res => res.data)
+// .then(data => {
+//   console.log(data)
+//   console.log(data.results[0])
+//   const formatted = data.results.map(item => new TickerCloseData({
+//     ticker: item.T,
+//     volume: item.v,
+//     volumeWeightedAerage: item.vw,
+//     open: item.o,
+//     close: item.c,
+//     highest:item.h,
+//     lowest:item.l,
+//     timeStart: item.t,
+//     numOfTrades: item.n
+//   }))
+
+// })
+
+  // TickerCloseData.collection.insertMany(formatted);
+
+
+
+
+  // if (response.data.results) {
+  //   console.log(response.data.results)
+  // }
+
+
+//   try {
+//     TickerCloseData.collection.insertMany(formatted);
+//   } catch(exception) {
+
+//   }
+//   // try {
+//   //   const savedData = await tickerData.save()
+//   //   response.status(201).json(savedData);
+//   // } catch(exception) {
+
+//   // }
+// })
+
+/*
+
+    ticker: {
+        type: String,
+        // required: true
+    },
+    volume: {
+        type: Number,
+        // required: true
+    },
+    volumeWeightedAerage: {
+        type: Number,
+        // required: true
+    },
+    open: {
+        type: Number,
+        // required: true
+    },
+    close: {
+        type: Number,
+        // required: true
+    },
+    highest: {
+        type: Number,
+        // required: true
+    },
+    lowest: {
+        type: Number,
+        // required: true
+    },
+    timeStart: {
+        type: Number,
+        // required: true
+    },
+    numOfTrades: {
+        type: Number,
+        // required: true
+    }
+
+*/
+// })
 
 // function httpGet(hostpath, chunkFunction) {
 //   https.get(hostpath, (res) => res.on('data', chunkFunction));
