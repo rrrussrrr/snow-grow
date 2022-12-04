f9d233a4b46d87c435778b5ab054bb7ca2b8f58a
rrrussrrr <rruebner@gmail.com>
2022-12-03 21:55:39 -0500

    working more on setting up interval for cache grab

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,17 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const tickerCloseDataRouter = require('./controllers/ticker_close_datapoints')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/ticker', tickerCloseDataRouter)
+
 
 module.exports = app
