4d5d7f8f874d18104850ed6443e3b2d1b3b0a57a
rrrussrrr <rruebner@gmail.com>
2022-12-04 20:10:52 -0500

    added check to see if username already exists

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -6,6 +6,14 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  // check if existing user
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return response.status(400).json({
+      error: 'username must be unique'
+    })
+  }
+
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
