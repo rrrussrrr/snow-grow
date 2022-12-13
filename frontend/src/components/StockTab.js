022705addef021453ed6664a4caa4223c8153ae0
rrrussrrr <rruebner@gmail.com>
2022-12-12 19:40:55 -0500

    now display percent change from last open to close

--- frontend/src/components/StockTab.js
+++ frontend/src/components/StockTab.js
@@ -3,24 +3,38 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { flexbox } from '@mui/system'
+import Box from '@mui/material/Box'
 
 const StockTab = ({stockData, toggleFavorite, loggedIn}) => {
+
+  const percentChange = Math.round(100 * (1 - (stockData.open / stockData.close)) * 10) / 10;
+
     return (
         <Card sx={{ minWidth: 275 }}>
         <CardContent>
           <Typography variant="h5" component="div">
             {stockData.ticker}
           </Typography>
-          <Typography variant="h5" component="div">
-            {stockData.close}
-          </Typography>
+
+          {stockData.close > stockData.open ?           
+            <Typography sx={{ color: "green"}} variant="h5" component="div">
+              Close: {stockData.close}
+              <div>Percent Change: {percentChange}%</div>
+            </Typography>
+          :
+            <Typography sx={{ color: "red"}} variant="h5" component="div">
+              Close: {stockData.close}
+              <div>Percent Change: {percentChange}%</div>
+            </Typography>
+          }
         </CardContent>
         <CardActions>
           {loggedIn ?
             <Button 
             size="small" 
             onClick={() => toggleFavorite(stockData.ticker)}
-            >Learn More
+            >Favorite
           </Button>
           : null
           }
