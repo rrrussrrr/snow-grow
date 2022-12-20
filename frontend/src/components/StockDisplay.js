8e2ce1f5944716618ebf20486304a3fa015cc703
rrrussrrr <rruebner@gmail.com>
2022-12-19 21:59:55 -0500

    changed stock table

--- frontend/src/components/StockDisplay.js
+++ frontend/src/components/StockDisplay.js
@@ -1,8 +1,14 @@
 import StockTab from "./StockTab"
-
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 const StockTable = ({stockList, toggleFavorite, loggedIn}) => {
     return (
         <div>
+            <Box>
+                <Typography>
+                    Your Stocks:
+                </Typography>
+            </Box>
             {stockList.map(stock => 
                 
                 <StockTab fave={true} toggleFavorite={toggleFavorite} loggedIn={loggedIn} stockData={stock}/>
